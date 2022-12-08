05126b39dc121fc26882848f3edaad968abcd435
Ann <niyta@mail.ru>
2022-12-08 16:06:05 +0500

    Initial commit

---
+++ src/main/java/sky/pro/questionforexamspring/QuestionForExamSpringApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.questionforexamspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionForExamSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionForExamSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/questionforexamspring/QuestionForExamSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.questionforexamspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionForExamSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
